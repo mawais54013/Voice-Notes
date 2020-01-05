0df780828ce4fc84d59c8f1fd1503aa924a12aba
MAwais <mawais54013@outlook.com>
2020-01-04 20:24:19 -0800

    Added Layout

---
+++ Voice Notes WatchKit App/NoteSelectRow.swift
@@ -0,0 +1,14 @@
+//
+//  NoteSelectRow.swift
+//  Voice Notes WatchKit Extension
+//
+//  Created by muhammad Awais on 1/4/20.
+//  Copyright © 2020 muhammad Awais. All rights reserved.
+//
+
+import WatchKit
+
+class NoteSelectRow: NSObject {
+
+    @IBOutlet var textLabel: WKInterfaceLabel!
+}
--- Voice Notes WatchKit Extension/InterfaceController.swift
+++ Voice Notes WatchKit Extension/InterfaceController.swift
@@ -12,6 +12,8 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var Table: WKInterfaceTable!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -27,5 +29,9 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    @IBAction func addNewNote() {
+        
+    }
+    
 }
