edf0a9314c541090db62871b8d3e81a7c20e001b
MAwais <mawais54013@outlook.com>
2020-01-04 20:11:17 -0800

    Initial commit

---
+++ Voice Notes WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Voice Notes WatchKit Extension
+//
+//  Created by muhammad Awais on 1/4/20.
+//  Copyright © 2020 muhammad Awais. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Voice Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voice Notes
+//
+//  Created by muhammad Awais on 1/4/20.
+//  Copyright © 2020 muhammad Awais. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
